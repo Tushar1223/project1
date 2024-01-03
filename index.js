9cf4125e5825f5564303161a62709fff69d7b646
Tushar Sharma <tushar007king@gmail.com>
2024-01-03 19:52:38 +0530

    first

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
 
-mongoose.connect("mongodb://localhost:27017/myLoginRegisterDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect("mongodb+srv://tushar2101125cs:JYnQA1uwhypgGiYy@cluster0.n3dxps6.mongodb.net/", {
+   
 }, () => {
-    console.log("DB connected")
+    console.log("DataBase connected successfully")
 })
 
 const userSchema = new mongoose.Schema({
@@ -20,7 +19,7 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-const User = new mongoose.model("User", userSchema)
+const User = new mongoose.model("Final project form", userSchema)
 
 //Routes
 app.post("/login", (req, res)=> {
@@ -61,6 +60,6 @@ app.post("/register", (req, res)=> {
     
 }) 
 
-app.listen(9002,() => {
-    console.log("BE started at port 9002")
+app.listen(4000,() => {
+    console.log("server started at port 4000")
 })
